f15843964f0160602ce7bb192dee560e4772c76b
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-03-02 11:46:41 -0600

    Step 4 + format (ERR)

--- src/CreateFilesBasedOnState.java
+++ src/CreateFilesBasedOnState.java
@@ -10,18 +10,19 @@ import java.text.*;
 public class CreateFilesBasedOnState {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        //3
+        // 3
         Path inStateFile = Paths.get("InStateCusts.txt");
         Path outOfStateFile = Paths.get("OutStateCusts.txt");
 
-        //4
+        // 4
         final String ID_FORMAT = "000";
         final String NAME_FORMAT = "          ";
         final int NAME_LENGTH = NAME_FORMAT.length();
         final String HOME_STATE = "WI";
         final String BALANCE_FORMAT = "0000.00";
         String delimiter = ",";
-        String s = ID_FORMAT + delimiter + NAME_FORMAT +delimiter + HOME_STATE + delimiter + BALANCE_FORMAT + System.getProperty("line.seperator");
+        String s = ID_FORMAT + delimiter + NAME_FORMAT + delimiter + HOME_STATE + delimiter + BALANCE_FORMAT
+                + System.getProperty("line.seperator");
         final int RECSIZE = s.length();
 
         FileChannel fcIn = null;
@@ -33,17 +34,22 @@ public class CreateFilesBasedOnState {
         double balance;
         final String QUIT = "999";
 
-        //These 2 Lines Will Cause Erorr!
+        // These 2 Lines Will Cause Erorr!
         createEmptyFile(inStateFile, s);
         createEmptyFile(outOfStateFile, s);
 
-
     }
+
     public static void createEmptyFile(Path file, String s){
         final int NUMRECS = 1000;
         try{
             OutputStream outputStr = new BufferedOutputStream(Files.newOutputStream(file, CREATE));
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStr));
+            for(int count = 0; count < NUMRECS; ++count){
+                writer.write(s, 0, s.length());
+            
+            }
+            writer.close();
         }
 
     }
