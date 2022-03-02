1f65925548dfb778859dde538f0c1952057fa1ec
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-03-02 11:27:15 -0600

    Step 4

--- src/CreateFilesBasedOnState.java
+++ src/CreateFilesBasedOnState.java
@@ -9,10 +9,21 @@ import java.text.*;
 public class CreateFilesBasedOnState {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
+        //3
         Path inStateFile = Paths.get("InStateCusts.txt");
         Path outStateFile = Paths.get("OutStateCusts.txt");
 
+        //4
+        final String ID_FORMAT = "000";
+        final String NAME_FORMAT = "          ";
+        final int NAME_LENGTH = NAME_FORMAT.length();
+        final String HOME_STATE = "WI";
+        final String BALANCE_FORMAT = "0000.00";
+        String delimiter = ",";
+        String s = ID_FORMAT + delimiter + NAME_FORMAT +delimiter + HOME_STATE + delimiter + BALANCE_FORMAT + System.getProperty("line.seperator");
+        final int RECSIZE = s.length();
 
+        
         
 
 
