b294b74c3a90be7a9d0ea9a4dba46e52e75254cc
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-03-03 11:45:25 -0600

    step 5-13

--- src/CreateFilesBasedOnState.java
+++ src/CreateFilesBasedOnState.java
@@ -10,19 +10,17 @@ import java.text.*;
 public class CreateFilesBasedOnState {
     public static void main(String[] args) throws Exception {
         Scanner input = new Scanner(System.in);
-        // 3
         Path inStateFile = Paths.get("InStateCusts.txt");
         Path outOfStateFile = Paths.get("OutStateCusts.txt");
 
-        // 4
+        
         final String ID_FORMAT = "000";
         final String NAME_FORMAT = "          ";
         final int NAME_LENGTH = NAME_FORMAT.length();
         final String HOME_STATE = "WI";
         final String BALANCE_FORMAT = "0000.00";
         String delimiter = ",";
-        String s = ID_FORMAT + delimiter + NAME_FORMAT + delimiter + HOME_STATE + delimiter + BALANCE_FORMAT
-                + System.getProperty("line.seperator");
+        String s = ID_FORMAT + delimiter + NAME_FORMAT + delimiter + HOME_STATE + delimiter + BALANCE_FORMAT + System.getProperty("line.separator");
         final int RECSIZE = s.length();
 
         FileChannel fcIn = null;
@@ -49,7 +47,30 @@ public class CreateFilesBasedOnState {
                 StringBuilder sb = new StringBuilder(name);
                 sb.setLength(NAME_LENGTH);
                 name = sb.toString();
+                System.out.print("Enter State >> ");
+                state = input.nextLine();
+                System.out.print("Enter balance >> ");
+                balance = input.nextDouble();
+                input.nextLine();
+                DecimalFormat df = new DecimalFormat(BALANCE_FORMAT);
+                s = idString + delimiter + name + delimiter + state + delimiter + df.format(balance) + System.getProperty("line.separator");
+                byte data[] = s.getBytes();
+                ByteBuffer buffer = ByteBuffer.wrap(data);
+                if(state.equals(HOME_STATE)){
+                    fcIn.position(id * RECSIZE);
+                    fcIn.write(buffer);
+                }
+                else{
+                    fcOut.position(id * RECSIZE);
+                    fcOut.write(buffer);
+                }
+                System.out.print("Enter next customer account number or " + QUIT + " to quit >> ");
+                idString = input.nextLine();
+                fcIn.close();
+                fcOut.close();
             }
+        }catch(Exception e){
+            System.out.println("Error Message: " + e);
         }
 
     }
@@ -61,7 +82,6 @@ public class CreateFilesBasedOnState {
             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(outputStr));
             for (int count = 0; count < NUMRECS; ++count) {
                 writer.write(s, 0, s.length());
-
             }
             writer.close();
         } catch (Exception e) {
