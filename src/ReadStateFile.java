23f03f5b10992fc09e30ccbd6b1aebd5ea32543c
Shlok Bhakta <shlokbhakta1@gmail.com>
2022-03-04 12:00:27 -0600

    Step 1-3

---
+++ src/ReadStateFile.java
@@ -0,0 +1,45 @@
+import java.nio.file.*;
+import java.io.*;
+import java.nio.file.attribute.*;
+import java.nio.channels.FileChannel;
+import java.nio.ByteBuffer;
+import static java.nio.file.StandardOpenOption.*;
+import java.util.Scanner;
+import java.text.*;
+
+public class ReadStateFile {
+
+    public static void main(String[] args) throws Exception {
+        Scanner kb = new Scanner(System.in);
+        String fileName;
+        System.out.print("Enter name of file to use >> ");
+        fileName = kb.nextLine();
+        fileName = fileName;//idk but the file lives in this project folder so uhh....
+        Path file = Paths.get(fileName);
+        
+        final String ID_FORMAT = "000";
+        final String NAME_FORMAT = "          ";
+        final int NAME_LENGTH = NAME_FORMAT.length();
+        final String HOME_STATE = "WI";
+        final String BALANCE_FORMAT = "0000.00";
+        String delimiter = ",";
+        String s = ID_FORMAT + delimiter + NAME_FORMAT + delimiter + HOME_STATE + delimiter + BALANCE_FORMAT + System.getProperty("line.separator");
+        final int RECSIZE = s.length();
+
+        
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+
+}
